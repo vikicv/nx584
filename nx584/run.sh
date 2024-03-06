4510f9bb3b2cd7b505b254a62552f8eb95caeeb9
vikicv <117547514+vikicv@users.noreply.github.com>
2024-03-06 18:23:25 +0100

    Update run.sh

--- nx584/run.sh
+++ nx584/run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/with-contenv bashio
-python3 /usr/bin/nx584_server --listen 0.0.0.0 --port 5007 --connect 192.168.3.54:5000 --config /usr/app/src/config.ini
+python3 /usr/bin/nx584_server --listen 0.0.0.0 --port 5007 --connect 192.168.3.62:5000 --config /usr/app/src/config.ini
